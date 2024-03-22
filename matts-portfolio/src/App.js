d57265380ec5ede02f5aed75c99e9c0940d91561
MattBarnabo <mattbarnabo@gmail.com>
2024-03-22 18:00:50 -0500

    removes text animation

--- matts-portfolio/src/App.js
+++ matts-portfolio/src/App.js
@@ -6,14 +6,10 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import MouseScroll from './components/MouseScroll';
 import Cursor from './components/Cursor';
-// import SplitType from 'split-type';
+
+
 
 const App = () => {
-  // let text = new SplitType('#text');
-  //     let characters = document.querySelectorAll('.char')
-  //     for(let i=0; i<characters.length; i++) {
-  //       characters[i].classList.add('translate-y-full')
-  //     }
   return (
     <>
     <div>
